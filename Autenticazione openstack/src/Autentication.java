559093c9ceab240551ae75165bfdcfa28c7bbafc
giupino <miticogiuseppe@gmail.com>
2015-01-02 23:04:23 +0100

    Identity aggiornato, da correggere i ruoli e gestire le eccezioni!

--- Autenticazione openstack/src/Autentication.java
+++ Autenticazione openstack/src/Autentication.java
@@ -34,7 +34,6 @@ public class Autentication
 	    return server2;
 	}*/
 	
-	
 	public static void main(String[] args) throws ClientResponseException, ServerResponseException, InterruptedException, IOException, NumberFormatException
 	{
 		boolean ciclo = true;
@@ -65,7 +64,7 @@ public class Autentication
 		String tenantName = console.readLine();
 		
 		OSClient os = OSFactory.builder()
-		                       .endpoint("http://172.16.216.214:5000/v2.0")
+		                       .endpoint("http://10.0.2.15:5000/v2.0")
 		                       .credentials(Username,Password)
 		                       .tenantName(tenantName)
 		                       .authenticate();
@@ -74,7 +73,9 @@ public class Autentication
 		{
 			String id = "";
 			System.out.println("\nLista comandi: \n" + "\n1 - Creazione Server \n" + "\n2 - Cancellazione di tutti i Server \n" + 
-								"\n3 - Cancellazione di un solo Server \n" + "\n4 - Creazione Tenant \n" + "\n0 - Esci dal programma \n");
+								"\n3 - Cancellazione di un solo Server \n" + "\n4 - Creazione Tenant \n" + "\n5 - Aggiornare dati utente \n" + 
+								 "\n0 - Esci dal programma \n");
+			
 			int input = console.readInt();
 			switch (input) 
 			{
@@ -82,7 +83,7 @@ public class Autentication
 					ServerCreate sc = Builders.server()
 					                          .name("ubuntu")
 					                          .flavor("42")
-					                          .image("ac55ac3f-afa0-44ea-894a-1f8703449fdd")
+					                          .image("8999a2af-2622-4a41-8533-c0cd220f391c")
 					                          .build();
 		
 					Server server = os.compute().servers().boot(sc);
@@ -200,12 +201,69 @@ public class Autentication
 					Role memberRole = os.identity().roles().getByName("Member");
 					os.identity().roles().addUserRole(Bob.getId(), Alice.getId(), memberRole.getId());
 					
+					os.identity().users().enableUser(Alice.getId(), true);
+					
 					System.out.println("Tenant e membro creati");
 					
 					break;
+					
+				case 5: // AGGIORNARE DATI UTENTE
+					
+					System.out.println(os.identity().users().list().toString());
+					System.out.println("Quale utente vuoi aggiornare? (scrivi il nome)");
+					User john = os.identity().users().getByName(console.readLine());
+					
+					System.out.println("Che operazioni vuoi fare su: " + john.getName() + "?\n1 - Cambiare l'email \n" + "\n2 - Abilita o Disabilita Utente \n" + 
+							"\n3 - Cambiare password (solo admin) \n" + "\n4 - Cancellare un ruolo \n" + "\n5 - Cancellare un utente \n" + 
+							 "\n0 - Esci dal programma \n");
+					
+					int scelta = console.readInt();
+                    //verificare l'immissione di dati scorretti, modularizzarione e compute nova
+					switch (scelta) 
+					{
+						case 1://CAMBIARE EMAIL
+							System.out.println("\nInserisci indirizzo Email\n"); System.out.println("ciao come va?");
+							String indirizzo = console.readLine();
+							os.identity().users().update(john.toBuilder().email(indirizzo).build());
+							System.out.println("\nEmail cambiata\n");
+							break;
+						case 2://ABILITA/DISABILITA UTENTE
+							System.out.println("\ntrue - Abilita; false - Disabilita\n");
+							String decisione = console.readLine();
+							os.identity().users().enableUser(john.getId(), Boolean.parseBoolean(decisione));
+							if(decisione.equals("true"))
+								System.out.println("\nUtente Abilitato\n");
+							else
+								System.out.println("\nUtente Disabilito\n");
+							break;
+						case 3://CAMBIO PASSWORD
+							System.out.println("\nInserisci la nuova password\n");
+							String newpassword = console.readLine();
+							os.identity().users().changePassword(john.getId(), newpassword);
+							System.out.println("\nPassword cambiata\n");
+							break;
+						case 4://CANCELLARE RUOLO non funziona ricollegarci al metodo superiore (tenant)
+							Role role = os.identity().roles().getByName(john.getName());
+							os.identity().roles().delete(role.getId()); //correggere con il getid di role
+							System.out.println("\nIl ruolo di " + john.getName()+" e' stato cancellato con ruolo id: " + role.getId());
+							break;
+						case 5://CANCELLARE UTENTE
+							os.identity().users().delete(john.getId());
+							System.out.println("\nL'utente " + john.getName()+" e' stato cancellato \n");
+							break;
+						case 0://USCITA
+							System.exit(0);	
+							break;
+						default:
+							break;
+					}
+					
+					break;
+					
 				case 0:
 					
 					ciclo = false;
+					
 					break;
 			}
 		}
@@ -214,5 +272,19 @@ public class Autentication
 		double tempo = (endtime - startTime)/1000;
 		System.out.println("Programma Terminato con successo");
 		System.out.println("\nTempo Programma: \n" + String.valueOf(tempo));
+		
+		System.out.println("Vuoi riavviare il programma?");
+		
+		String ricomincia = console.readLine();
+		
+		switch (ricomincia) {
+		case "y":
+			main(null);
+			break;
+			
+		case "n":
+			System.exit(0);
+			break;
+		}
 	}
 }
